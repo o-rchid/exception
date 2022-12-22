12dddc13baf9a773954d2bcf0389240479c92a60
o-rchid <ozzve35@gmail.com>
2022-12-22 09:57:22 +0900

    API 예외처리

---
+++ src/main/java/hello/exception/api/ApiExceptionV3Controller.java
@@ -0,0 +1,37 @@
+package hello.exception.api;
+
+import hello.exception.exception.UserException;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ApiExceptionV3Controller {
+
+    @GetMapping("/api3/members/{id}")
+    public Memberdto getMember(@PathVariable("id") String id) {
+
+        if (id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+        if (id.equals("bad")) {
+            throw new IllegalArgumentException("잘못된 입력 값");
+        }
+        if (id.equals("user-ex")) {
+            throw new UserException("사용자 오류");
+        }
+
+        return new Memberdto(id, "hello" + id);
+    }
+
+    @Data
+    @AllArgsConstructor
+    static class Memberdto {
+        private String memberId;
+        private String name;
+    }
+}
